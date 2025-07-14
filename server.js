0c25d05e5681ab8cfd91e9acca4b8bf550110da9
Arpan Patra <arpanpatra800188500@gmail.com>
2025-07-14 22:28:18 +0530

    Update server.jsPortChange

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ app.get("/weather", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
