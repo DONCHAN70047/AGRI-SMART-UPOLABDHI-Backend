be8008d0cd54c3ba78ced611682e7dfeaa34d518
Arpan Patra <arpanpatra800188500@gmail.com>
2025-07-26 20:56:47 +0530

    Update MLapp.py1.1

--- upolabdhi_backend/backend_router/MLModel/MLapp.py
+++ upolabdhi_backend/backend_router/MLModel/MLapp.py
@@ -1,29 +1,39 @@
 import numpy as np
 import pickle
+import gdown
+import io
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import os
-import google.generativeai as genai    # RT-Import : google-generativeai
-
+import google.generativeai as genai
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+file_id = "1JrIvT1aTT0cOo52ARHZskDI15WIXVrxn"
+url = f"https://drive.google.com/uc?id={file_id}"
 
-model_path = os.path.join(BASE_DIR, "DisesDetectModel16.keras")
-model = load_model(model_path)
+model_bytes = io.BytesIO()
+gdown.download(url, output=model_bytes, quiet=False, fuzzy=True)
+
+model_bytes.seek(0)
+
+with open("temp_model.h5", "wb") as f:
+    f.write(model_bytes.read())
+
+model = load_model('temp_model.h5')
+os.remove("temp_model.h5")
 
 
 class_map_path = os.path.join(BASE_DIR, "ClassMapDisesDetectModel16.pkl")
 with open(class_map_path, 'rb') as f:
     class_indices = pickle.load(f)
-
 index_to_class = {v: k for k, v in class_indices.items()}
 
-def Sugesstion(DisesName) :
-    genai.configure(api_key=os.getenv("DisesSugesstionAPIKey")
-)
+
+def Sugesstion(DisesName):
+    genai.configure(api_key=os.getenv("DisesSugesstionAPIKey"))
     model = genai.GenerativeModel(model_name="models/gemini-1.5-flash")
-    response = model.generate_content(f"Our dises name is : {DisesName}  so, you give me point wise sugesstion of  how to came dises & how to servive from this diises..")
+    response = model.generate_content(f"Our disease name is: {DisesName}. Please give me point-wise suggestions on how the disease happens and how to survive from it.")
     return response.text
 
 
@@ -36,11 +46,9 @@ def predict_disease(image_file):
     predicted_index = np.argmax(predictions[0])
     predicted_class = index_to_class[predicted_index]
 
-    PredictDisesSugesstion = Sugesstion(predicted_class)
-
-
+    suggestion_text = Sugesstion(predicted_class)
 
     return {
         "disease": predicted_class,
-        "suggestion": [PredictDisesSugesstion]
+        "suggestion": [suggestion_text]
     }
