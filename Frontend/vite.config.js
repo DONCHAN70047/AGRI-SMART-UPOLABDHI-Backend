d047da498b9ea7f144ebbe3ebd3b101836ebeb40
Arpan Patra <arpanpatra800188500@gmail.com>
2025-07-28 01:31:33 +0530

    Update vite.config.js1.2

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,18 +2,14 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
     port: process.env.PORT || 5173,
     
-    // ✅ Add this block to allow backend host
-    allowedHosts: [
-      'localhost',
-      'https://agri-smart-upolabdhi-backend-8.onrender.com'
-    ],
+    // ✅ Add this line:
+    allowedHosts: ['agri-smart-upolabdhi-backend-xbz4.onrender.com']
   },
   preview: {
     host: true,
